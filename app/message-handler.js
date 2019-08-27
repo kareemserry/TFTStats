b85e90fc15db584a3621a960615f455fc3d217b4
kareemserry <kareem-serry@hotmail.com>
2019-08-28 01:01:49 +0200

    added ping! pong! functionality on messages that start with hello! for initial deployment

--- app/message-handler.js
+++ app/message-handler.js
@@ -4,6 +4,8 @@ const logger = require('../utils/logger.js')(module.filename);
 
 handle = msg => {
     logger.silly(`Recieved Message: '${msg.author.username}: ${msg.content}'`);
+
+    if (msg.content.split(" ")[1] == "hello!") msg.reply("Hi!");
 }
 
 exports.handle = handle
